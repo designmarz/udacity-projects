63d74b9c6f08f434c2882db0552a98e2816bf078
Nick Marazzo <designmarz@gmail.com>
2016-01-25 10:51:39 -0800

    added owl carousel

--- FE Nanodegree Project 1/js/app.js
+++ FE Nanodegree Project 1/js/app.js
@@ -3,60 +3,46 @@ $(document).ready(function() {
 // console.log("Doc Loading");
 	
 	$.getJSON( "data/content.json", function( data ) {
-
 		var key, count = 0;
-		
 		for(key in data) { if(data.hasOwnProperty(key)) { count++; }
 		  // data.length = count;
 		}
-
 		gloData = data; // jshint ignore:line
 		console.log(data);
 		console.log(count);
-
-
-
-	var html = '';
-	// var html = '<div class="test">';
+		var html = '';
 		$.each( data, function( key, val ) {
-			console.log("Key -----------");
-			console.log(key);
-			console.log("----------- Value");
-			console.log(val);
-			console.log("------ End -----");
-
-			html += "<article class='gallery_image col-sm-2'>";
-			// split line into columns
-			
+			// console.log("------ Loop Start -----");
+			// console.log("Key   -----------> " + key);
+			// console.log("Value -----------> " + val);
+			// console.log("------ Loop End -----");
+			html += "<article class='gallery_image'>";
 			html += "<header><h1 class='primary-text-color'>" + val[0] + "</h1></header>";
-			html += "<a href='#' class='thumbnail'><img src='" + val[2] + "' id='"+key+"' class='img-responsive'alt='"+val+"'></a>";
+			// html += "<a href='#' class=''>";
+			html += "<img src='" + val[2] + "' id='"+key+"' class='img-responsive'alt='"+val+"'>";
+			// html += "</a>";
 			html += "<footer><p class='secondary-text-color'>" + val[1] + "</p></footer>";
 			// close row
 			html += "</article>";		
 		});
-
-
-		// insert into div
+		// insert html into div
 		$('#gallery').append(html);
-	});
 
+		// owl-carousel Start //
 
+		$('.owl-carousel').owlCarousel({
+			    animateOut: 'slideOutDown',
+			    animateIn: 'flipInX',
+			    items:2,
+			    margin:10,
+			    stagePadding:30,
+			    smartSpeed:450
+			});
 
-//   $.getJSON( "data/content.json", function( data ) {
-//   var items = [];
+		// owl-carousel End //
 
-//   $.each( data, function( key, val ) {
-//     items.push( "<li id='" + key + "'>" + val[0] + "</li>" );
-//     items.push( "<li id='" + key + "'>" + val[2] + "</li>" );
-//     items.push( "<li id='" + key + "'>" + val[1] + "</li>" );
-//   });
- 
-//   $( "<ul/>", {
-//     "class": "my-new-list",
-//     html: items.join( "" )
-//   }).appendTo( "#landing" );
-// });
+		console.log("JSON Loaded");
+	}); // End JSON data //
 
-
-// console.log("Doc Loaded");
+console.log("Doc Loaded");
 });
